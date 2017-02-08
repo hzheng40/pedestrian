7760e076f91e5f431f6ba07ae012de74bcfb56b8
Hongrui Zheng <billyzheng@lawn-128-61-59-60.lawn.gatech.edu>
2017-02-08 14:30:54 -0500

    adding haar cascade

--- scripts/subimage.py
+++ scripts/subimage.py
@@ -14,20 +14,36 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class image_converter:
 	def __init__(self):
+		# subscribers, bridge and publisher init
 		self.image_sub = rospy.Subscriber('/camera/rgb/image_color', Image, self.callback)
 		self.bridge = CvBridge()
 		self.image_pub = rospy.Publisher('results', Image, queue_size=10)
+
+		# using hog descriptor to detect human
 		self.HOG = cv2.HOGDescriptor()
 		self.HOG.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+		# using haar cascade to detect human
+		self.cascade = cv2.CascadeClassifier('haarcascade_pedestrian.xml')
 
 	def callback(self, data):
 		try:
 			cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
 		except CvBridgeError as e:
 			print(e)
+
+		# for haar cascade
+		# TODO: finish detection with haar cascade
+		cv_gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+		self.cascade.detectMultiScale(cv_gray)
+
+		# for hog descriptor
+		# TODO: try different scales to find improved speed
 		cv_image = imutils.resize(cv_image, width=min(400, cv_image.shape[1]))
 		(rects, weights) = self.HOG.detectMultiScale(cv_image, winStride=(4,4), padding=(8,8), scale=1.05)
 		rects = np.array([[x,y,x+w,y+h] for (x,y,w,h) in rects])
+
+		# non max suppression
 		pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 		for(xA, yA, xB, yB) in pick:
 			cv2.rectangle(cv_image, (xA,yA), (xB, yB), (0,255,0), 2)
