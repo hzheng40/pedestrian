895b4c23d55466762d71ed4b1cdb1ec2899de804
Hongrui Zheng <billyzheng.bz@gmail.com>
2017-03-14 18:09:45 -0400

    update

---
+++ scripts/slam.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# gmapping subscribes to tf and scan, publishes to map_metadata, map, ~entropy
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
+from nav_msgs.msg import MapMetaData, OccupancyGrid#!/usr/bin/env python
+
+
+class slam:
+	def __init__(self):
+		self.map_pub = rospy.Publisher('map', OccupancyGrid, queue_size=10)
+
+
+
+
+
+	def 
