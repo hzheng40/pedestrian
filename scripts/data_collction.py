cb157e083ed4077b555063ba50828846d85a4ba6
Hongrui Zheng <billyzheng.bz@gmail.com>
2017-03-23 00:13:14 -0400

    update data collection

--- scripts/data_collction.py
+++ scripts/data_collction.py
@@ -1,66 +1,79 @@
 #!/usr/bin/env python
 
+# collecting data from turtlebot (image, human input)
+# frame and corresponding instruction.
+
 from __future__ import print_function
 import rospy
 from geometry_msgs.msg import Twist
 import sys, select, termios, tty
 
-msg="""
-Control Your Turtlebot!
----------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-space key, k : force stop
-anything else : stop smoothly
-CTRL-C to quit
-"""
+import rosbag
+from std_msgs.msg import Int32, String
+from sensor_msgs.msg import Image
 
-moveBindings = {
-	'i':(1,0),
-	'o':(1,-1),
-	'j':(0,1),
-	'l':(0,-1),
-	'u':(1,1),
-	',':(-1,0),
-	'.':(-1,1),
-	'm':(-1-1)
-}
+class data_collectors:
+	def __init__(self):
+		self.image_sub = rospy.Subscriber('/camera/rgb/image_color', Image, self.callback)
+		self.msg="""
+				Control Your Turtlebot!
+				---------------------------
+				Moving around:
+				   u    i    o
+				   j    k    l
+				   m    ,    .
+				q/z : increase/decrease max speeds by 10%
+				w/x : increase/decrease only linear speed by 10%
+				e/c : increase/decrease only angular speed by 10%
+				space key, k : force stop
+				anything else : stop smoothly
+				CTRL-C to quit
+				"""
+		self.moveBindings = {
+							'i':(1,0),
+							'o':(1,-1),
+							'j':(0,1),
+							'l':(0,-1),
+							'u':(1,1),
+							',':(-1,0),
+							'.':(-1,1),
+							'm':(-1-1)
+							}
+		self.speedBindings={
+							'q':(1.1,1.1),
+							'z':(.9,.9),
+							'w':(1.1,1),
+							'x':(.9,1),
+							'e':(1,1.1),
+							'c':(1,0.9)
+							}
+		self.bag = rosbag.Bag('test.bag', 'w')
+		self.speed = .2
+		self.turn = 1
 
-speedBindings={
-	'q':(1.1,1.1),
-	'z':(.9,.9),
-	'w':(1.1,1),
-	'x':(.9,1),
-	'e':(1,1.1),
-	'c':(1,0.9)
-}
+	def getKey():
+		tty.setraw(sys.stdin.fileno())
+		rlist,_, _ = select.select([sys.stdin],[],[],0.1)
+		if rlist:
+			key=sys.stdin.read(1)
+		else:
+			key=''
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+		return key
 
-def getKey():
-	tty.setraw(sys.stdin.fileno())
-	rlist,_, _ = select.select([sys.stdin],[],[],0.1)
-	if rlist:
-		key=sys.stdin.read(1)
-	else:
-		key=''
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+	def vels(speed, turn):
+		return "currently:\tspeed %s\tturn %s " % (self.speed, self.turn)
 
-speed = .2
-turn = 1
+	def callback(self, data):
+		self.bag.write('image', data)
+		self.bag.write('instruction', self.getKey())
 
-def vels(speed, turn):
-	return "currently:\tspeed %s\tturn %s " % (speed, turn)
 
-if __name__ == '__main__':
+def main(args):
+	dc = data_collectors()
 	settings = termios.tcsetattr(sys.stdin)
-	rospy.init_node('turtlebot_data_collect')
-	pub = rospy.Publisher('~cmd_vel', Twist, queue_size=5)
-
+	rospy.init_node('turtlebot_data_collect', anonymous=True)
+	pub = rospy.Publisher('~cmd_vel', Twits, queue_size=5)
 	x = 0
     th = 0
     status = 0
@@ -137,3 +150,8 @@ if __name__ == '__main__':
         pub.publish(twist)
 
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    self.bag.close()
+
+
+if __name__ == '__main__':
+	main(sys.argv)
